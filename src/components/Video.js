5d9c52de588e85623b05fd599e4a36c1485f55db
devenes <ahmedenesturan@gmail.com>
2022-01-07 18:32:11 +0300

    messagingContainer added

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,5 +1,18 @@
+import ReactPlayer from "react-player";
+
 const Video = () => {
-  return <div className="video.container"></div>;
+  return (
+    <div className="video-container">
+      <ReactPlayer
+        url={"https://www.youtube.com/watch?v=63x11iR5gjo"}
+        muted={true}
+        controls={false}
+        playing={true}
+        width={"100%"}
+        height={"100%"}
+      />
+    </div>
+  );
 };
 
 export default Video;
