43ea6363b4939e3d8b2f8a4ef7efa75d350532ea
devenes <ahmedenesturan@gmail.com>
2022-01-16 23:36:14 +0300

    Style update

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,73 +1,77 @@
-import { useState } from "react";
+import React, { Component }  from 'react';
+import {useState} from 'react'
+import axios from 'axios'
+import { useCookies } from 'react-cookie'
 
-const Auth = ({}) => {
-  const [isLogin, setIsLogin] = useState(true);
-  const [username, setUsername] = useState(null);
-  const [password, setPassword] = useState(null);
-  const [confirmPassword, setConfirmPassword] = useState(null);
-  const [error, setError] = useState(false);
+const Auth = () => {
+    const [cookies, setCookie, removeCookie] = useCookies(['user'])
+    const [isLogin, setIsLogin] = useState(true)
+    const [username, setUsername] = useState(null)
+    const [password, setPassword] = useState(null)
+    const [confirmPassword, setConfirmPassword] = useState(null)
+    const [error, setError] = useState(false)
 
-  console.log(username);
-  console.log(password);
+    const handleSubmit = async (endpoint) => {
+        console.log(endpoint)
+        if (!isLogin && password !== confirmPassword) {
+            setError(true)
+            return
+        }
 
-  const handleSubmit = () => {
-    console.log("submitted");
-    if (password !== confirmPassword) {
-      setError(true);
-      return;
+        const response = await axios.post(`http://localhost:8000/${endpoint}`, {
+            username,
+            password
+        })
+
+        setCookie('Name', response.data.username)
+        setCookie('HashedPassword', response.data.hashedPassword)
+        setCookie('UserId', response.data.userId)
+        setCookie('AuthToken', response.data.token)
+
+        window.location.reload()
     }
-  };
-  return (
-    <div className="auth-container">
-      <div className="auth-container-box">
-        <div className="auth-container-form">
-          <input
-            type="text"
-            id="username"
-            name="username"
-            placeholder="username"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <input
-            type="text"
-            id="password"
-            name="password"
-            placeholder="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          {!isLogin && (
-            <input
-              type="text"
-              id="password-check"
-              name="password-check"
-              placeholder="confirm password"
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-          )}
-          {error && <p>Make sure the passwords match</p>}
-          <button className="standard-button" onClick={handleSubmit}>
-            GO!
-          </button>
-        </div>
-        <div className="auth-options">
-          <button
-            onClick={() => setIsLogin(false)}
-            style={{
-              backgroundColor: !isLogin ? "#1D162E" : "#070a0d",
-            }}
-          >
-            Sign up
-          </button>
-          <button
-            onClick={() => setIsLogin(true)}
-            style={{ backgroundColor: isLogin ? "#1D162E" : "#070a0d" }}
-          >
-            Login
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-};
 
-export default Auth;
+    return (
+        <div className="auth-container">
+            <div className="auth-container-box">
+                <div className="auth-container-form">
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        placeholder="username"
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        id="password"
+                        name="password"
+                        placeholder="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    {!isLogin && <input
+                        type="text"
+                        id="password-check"
+                        name="password-check"
+                        placeholder="confirm password"
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    />}
+                    {error && <p>* Make sure passwords match</p>}
+                    <button className='standard-button' onClick={() => handleSubmit(isLogin ? 'login' : 'signup')}>GO!</button>
+                </div>
+                <div className="auth-options">
+                    <button
+                        onClick={() => setIsLogin(false)}
+                        style={{ backgroundColor: !isLogin ? '#151a1f' : '#070a0d'}}
+                    >Sign up</button>
+                    <button
+                        onClick={() => setIsLogin(true)}
+                        style={{ backgroundColor: isLogin ? '#151a1f' : '#070a0d'}}
+                    >Login</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Auth
--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -1,19 +1,19 @@
-import ReactPlayer from "react-player";
+import React, { Component }  from 'react';
+import ReactPlayer from 'react-player'
 
 const Video = () => {
-  return (
-    <div className="video-container">
-      <ReactPlayer
-        url={"https://www.youtube.com/watch?v=63x11iR5gjo"}
-        //for test
-        muted={true}
-        controls={false}
-        playing={true}
-        width={"100%"}
-        height={"100%"}
-      />
-    </div>
-  );
-};
+    return (
+        <div className="video-container">
+            <ReactPlayer
+                url={"https://www.youtube.com/watch?v=PBrEq9Wd6_U"}
+                muted={true}
+                controls={false}
+                playing={true}
+                width={'100%'}
+                height={'100%'}
+            />
+        </div>
+    )
+}
 
-export default Video;
+export default Video
