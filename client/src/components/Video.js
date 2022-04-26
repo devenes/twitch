3dcf483755293adc3cf6926c7aaa4599d765a11f
Enes <66560757+devenes@users.noreply.github.com>
2022-04-27 01:01:51 +0300

    Update Video.js

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -1,19 +1,18 @@
-import React, { Component }  from 'react';
-import ReactPlayer from 'react-player'
+import ReactPlayer from "react-player";
 
 const Video = () => {
-    return (
-        <div className="video-container">
-            <ReactPlayer
-                url={"https://www.youtube.com/watch?v=PBrEq9Wd6_U"}
-                muted={true}
-                controls={false}
-                playing={true}
-                width={'100%'}
-                height={'100%'}
-            />
-        </div>
-    )
-}
+  return (
+    <div className="video-container">
+      <ReactPlayer
+        url={"https://youtu.be/hUxZZKsL1b0"}
+        muted={false}
+        controls={false}
+        playing={true}
+        width={"100%"}
+        height={"100%"}
+      />
+    </div>
+  );
+};
 
-export default Video
+export default Video;
